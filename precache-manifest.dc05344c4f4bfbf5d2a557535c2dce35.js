4a7274202de1cec7e64b8ff12315b634a15c9e02
Alex Dushevski <39350701+Tehheavy@users.noreply.github.com>
2020-04-14 10:55:59 +0300

    Updates

---
+++ precache-manifest.dc05344c4f4bfbf5d2a557535c2dce35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b871ad023d7e65a13b8cbdcb335c7f",
+    "url": "/puzzle-box-single/index.html"
+  },
+  {
+    "revision": "47aeebfce9e669a491a8",
+    "url": "/puzzle-box-single/static/css/main.5d72fbc6.chunk.css"
+  },
+  {
+    "revision": "78b866f28899708fac38",
+    "url": "/puzzle-box-single/static/js/2.67e0af72.chunk.js"
+  },
+  {
+    "revision": "fdcdf440bb62a54b154e44fab8465268",
+    "url": "/puzzle-box-single/static/js/2.67e0af72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aeebfce9e669a491a8",
+    "url": "/puzzle-box-single/static/js/main.df5359e5.chunk.js"
+  },
+  {
+    "revision": "b67c2bc5ce2fd1f67a92",
+    "url": "/puzzle-box-single/static/js/runtime-main.050333be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puzzle-box-single/static/media/logo.5d5d9eef.svg"
+  }
+]);
